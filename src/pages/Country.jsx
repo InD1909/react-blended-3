80518faaa7329f206b70800fc7a1b5526812efda
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-07-26 21:50:11 +0300

    task-3

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,29 @@
-import { Container, Heading, Section } from 'components';
+import { Container, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/countryApi';
+import { CountryInfo } from 'components';
 
 export const Country = () => {
+  const { countryId } = useParams();
+  const [country, setCountry] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchCountry(countryId)
+      .then(data => setCountry(data))
+      .catch(error => setError(error.message))
+      .finally(setLoading(false));
+  }, [countryId]);
+
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        {country && <CountryInfo {...country} />}
+        {loading && <Loader />}
+        {error && <Heading title={error} />}
       </Container>
     </Section>
   );
